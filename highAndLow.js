3944c6be61904f55ce55995e0ec368611d587f26
Adrian290404 <adrianmg2004.29@gmail.com>
2025-01-16 11:25:22 +0100

    high and low function

---
+++ highAndLow.js
@@ -0,0 +1,6 @@
+function highAndLow(numbers){
+    let numArray = numbers.split(" ").map(Number);
+    let max = Math.max(...numArray);
+    let min = Math.min(...numArray);
+    return max + " " + min;
+}
